72a0754c8e0b2a459154d9fa9a6dcb2f66805e48
Leslie Mejia <lesliemejia110316@gmail.com>
2024-11-21 22:08:28 -0800

    finished heart button js

--- Etsy-Server/static/JS-files/productpage.js
+++ Etsy-Server/static/JS-files/productpage.js
@@ -15,7 +15,6 @@ mainImage.src = images[index].src;
 mainImage.alt = images[index].alt;
 }
 
-// Scroll to the selected image in the vertical carousel
 function scrollToImage(index) {
 const selectedImage = images[index];
 selectedImage.scrollIntoView({
@@ -55,15 +54,26 @@ updateMainImage(0);
 
 
 //heart button fill
-document.querySelector('.carousel-button').addEventListener('click', function () {
-  const heart = this.querySelector('.heart path'); // Select the <path> inside the SVG
-  const currentFill = heart.style.fill;
+const button = document.querySelector('.carousel-button');
 
-  if (currentFill === 'red') {
-    heart.style.fill = '';
-  } else {
-    heart.style.fill = 'red'; 
-  }
+const heartSVG1 = `
+    <svg class="heart1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+        <path d="M12,21C10.349,21,2,14.688,2,9,2,5.579,4.364,3,7.5,3A6.912,6.912,0,0,1,12,5.051,6.953,6.953,0,0,1,16.5,3C19.636,3,22,5.579,22,9,22,14.688,13.651,21,12,21ZM7.5,5C5.472,5,4,6.683,4,9c0,4.108,6.432,9.325,8,10,1.564-.657,8-5.832,8-10,0-2.317-1.472-4-3.5-4-1.979,0-3.7,2.105-3.721,2.127L11.991,8.1,11.216,7.12C11.186,7.083,9.5,5,7.5,5Z"></path>
+    </svg>
+`;
+const heartSVG2 = `
+    <svg class="heart2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+        <path d="M16.5,3A6.953,6.953,0,0,0,12,5.051,6.912,6.912,0,0,0,7.5,3C4.364,3,2,5.579,2,9c0,5.688,8.349,12,10,12S22,14.688,22,9C22,5.579,19.636,3,16.5,3Z"></path>
+    </svg>
+`;
+
+button.addEventListener('click', function () {
+    if (button.innerHTML.includes('M12,21')) {
+        button.innerHTML = heartSVG2; 
+    } else {
+        button.innerHTML = heartSVG1; 
+    }
 });
+
 
 
