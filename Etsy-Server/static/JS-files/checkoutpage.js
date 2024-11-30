713094185994ab4394a9dc8a83151f46de52b6b0
Your Name <you@example.com>
2024-11-30 12:48:25 -0700

    finished cart remove

--- Etsy-Server/static/JS-files/checkoutpage.js
+++ Etsy-Server/static/JS-files/checkoutpage.js
@@ -1,27 +1,18 @@
 
 
-// function myFunction() {
-//     const element = document.querySelector(".wholecart");
-//     element.remove();
-// }
-
-// function toggleContent() {
-//     const hiddenParagraphs = document.querySelectorAll('.more');
-//     const button = document.querySelector('.cart-button2'); 
-//     hiddenParagraphs.forEach(p => {
-//     p.classList.toggle('visible');
-//     });
-// }
 
 
 
 function removeCartItem() {
     console.log("remainingItems");
     // Select the cart item and the empty cart message element
-    const element = document.querySelector('.wholecart');
-    const emptyCartMessage = document.getElementById('emptyCartMessage');
+    const element = document.querySelector('.cart-item');
+    const checkout = document.querySelector('.checkout');
     const cartContainer = document.querySelector('.wholecart');
 
+    if (checkout) {
+        checkout.remove();
+    }
     // Remove the cart item from the DOM
     if (element) {
         element.remove();
@@ -29,12 +20,13 @@ function removeCartItem() {
 
     // // Check if the cart is now empty
     const remainingItems = cartContainer.querySelectorAll('.wholecart');  
-    console.log("remainingItems");
-    // if (remainingItems.length === 0) {
-    //     emptyCartMessage.style.display = 'block';
-    // }
-    if (emptyCartMessage.length === 0) {
-        emptyCartMessage.style.display = 'block';
+    console.log(remainingItems);
+    if (remainingItems.length === 0) {
+        document.getElementById('emptyCartMessage').style.display = 'block';
+        document.getElementById('header').style.display = 'block';
+        // document.getElementById('checkout').style.display = 'block';
+        console.log("executed visibility")
     }
-    console.log("Cart is empty, showing empty message");
+
+
 }
